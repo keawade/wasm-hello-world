02039d7456a803fc49f6428829fe1788f1170fd0
Keith Wade <keawade@gmail.com>
2017-06-11 14:03:05 -0500

    Counter on screen

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import Counter from './wasm/counter.wasm'
 
-const wasmHelloWorld = () => {
-  const counter = new Counter({
-    'env': {
-      'memoryBase': 0,
-      'tableBase': 0,
-      'memory': new WebAssembly.Memory({ initial: 256 }),
-      'table': new WebAssembly.Table({ initial: 0, element: 'anyfunc' }),
-    },
-  });
-  console.log('count function result is: ', counter.exports._count());
-}
+const counter = new Counter({
+  'env': {
+    'memoryBase': 0,
+    'tableBase': 0,
+    'memory': new WebAssembly.Memory({ initial: 256 }),
+    'table': new WebAssembly.Table({ initial: 0, element: 'anyfunc' }),
+  },
+});
 
-window.onload = wasmHelloWorld;
+const countText = document.createElement('div');
+countText.textContent = counter.exports._count();
+
+const incFunc = () => {
+  const tempValue = counter.exports._count();
+  console.log('count: ', tempValue);
+  countText.textContent = tempValue;
+};
+
+const anchor = document.getElementById('anchor');
+
+const incButton = document.createElement('button');
+incButton.textContent = 'Execute';
+incButton.onclick = incFunc;
+
+anchor.appendChild(countText)
+anchor.appendChild(incButton);
